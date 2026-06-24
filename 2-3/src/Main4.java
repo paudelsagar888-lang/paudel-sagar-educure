0a80e75a39b2121df3284942c5eb9fb6c8849834
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-06-24 18:09:02 +0900

    Fix typo in Java programming message

--- 2-3/src/Main4.java
+++ 2-3/src/Main4.java
@@ -1,5 +1,5 @@
 public class Main4 {
     public static void main(String[]args) {
-        System.out.println("彼は\"javaProgramming\"が好きです。\nつぎの行です。\n\tタブを使用しています。");
+        System.out.println("彼は\"Java Programming\"が好きです。\n次の行です。\n\tタブを使用しています。");
     }
 }
