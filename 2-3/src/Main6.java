883cb20f9e7c340ad848bd298595ba636784e9d7
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-06-24 21:38:28 +0900

    Refactor price output format in Main6.java

--- 2-3/src/Main6.java
+++ 2-3/src/Main6.java
@@ -1,9 +1,9 @@
 public class Main6 {
     public static void main(String[] args) {
-        int pricebefore = 1000;
-        System.out.println("変更前の価格:" + pricebefore);
-        int priceafter= 1200;
-        System.out.println("変更後の価格:" + priceafter);
+        int price = 1000;
+        System.out.println("変更前の価格: " + price +"円");
+        price = 1200;
+        System.out.println("変更後の価格: " + price +"円");
     }
     
 }
