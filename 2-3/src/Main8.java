2a76746fd77b5f6c217db9d13843c2b7ea53a49d
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-06-24 18:10:57 +0900

    Fix typo in self-introduction program comments

--- 2-3/src/Main8.java
+++ 2-3/src/Main8.java
@@ -1,17 +1,18 @@
 /**
-        *自己紹介を表示するプログラミングです 
+        *自己紹介を表示するプログラムです 
         */
 public class Main8 {
     public static void main(String[] args) { 
         //自己紹介を情報として表示する
        System.out.println("===自己紹介===");
-       
+
        String lastName ="パウデル";//姓
        String firstName ="サガール";//名
-
+       String homeTown ="ネパール";//出身地
+       String favoriteFood ="モモ";//好きな食べ物
        System.out.println("名前:"  +lastName + " " + firstName);
-       System.out.println("出身地: ネパール");
-       System.out.println("好きな食べ物: 鶏肉");
+       System.out.println("出身地:" + homeTown);
+       System.out.println("好きな食べ物:" + favoriteFood);
     }
     
 }
