c8e820cd8febc516d0bf9d41fffb742351297d00
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-06-28 16:22:01 +0900

    Update salad weight for price calculation

--- 2-7/Main4.java
+++ 2-7/Main4.java
@@ -1,8 +1,8 @@
 public class Main4 {
     public static void main(String[]args){
         int saladPriceper100g = 138;
-        double saladWeight = 100.0;
-        
+        double saladWeight = 315.8;
+
         double saladPrice = saladPriceper100g * saladWeight / 100;
         int saladPriceInt = (int)saladPrice;
         System.out.println("合計金額: " + saladPriceInt + "円です。");
