9a68d9185461ac35ef9da19a938ceca548ae14db
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-06-28 16:22:21 +0900

    Update quantities and change total cash amount

--- 2-7/Main5.java
+++ 2-7/Main5.java
@@ -1,13 +1,13 @@
 public class Main5 {
     public static void main (String[]args){
-       int costofNotebook = 280 * 2 ;
-       int costofPencils = 120 * 3 ;
-       int costofErasers = 100 * 1 ;
+       int costofNotebook = 280 * 3 ;
+       int costofPencils = 120 * 5 ;
+       int costofErasers = 100 * 2 ;
 
        int TotalCost = costofNotebook + costofPencils + costofErasers ;
        double Tax = (TotalCost * 0.1);
        int roundedTax= (int)Tax;
-       int change = 1500 - (TotalCost + roundedTax);
+       int change = 2000 - (TotalCost + roundedTax);
 
        System.out.println("小計: " + TotalCost + "円");
        System.out.println("消費税: " + roundedTax + "円");
