9f611d63a76ba217e891e79eb4d606e76ca2beaf
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-06-28 16:21:12 +0900

    Update defencePower value in damage calculation
    Changed defencePower from 15 to 5 to adjust damage calculation.

--- 2-7/Main3.java
+++ 2-7/Main3.java
@@ -1,8 +1,8 @@
 public class Main3 {
     public static void main(String[]args){
         int attackPower = 15;
-        int defencePower =15;
-        int damage = (int)(attackPower * 2 - defencePower)/3;
+        int defencePower = 5;
+        int damage = (int)(attackPower * 2 - defencePower)/3; // 整数同士で計算すると、小数点以下は切り捨てられる
         System.out.println("与えるダメージ: " + damage);
     }
     
