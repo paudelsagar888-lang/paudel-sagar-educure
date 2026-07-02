e8c3e44389f47db65e013fd8c24daa07f80e2dbc
paudelsagar888-lang <paudelsagar888@gmail.com>
2026-07-02 09:22:03 +0900

    Change month initialization and update season checks

--- 2-10/Main3.java
+++ 2-10/Main3.java
@@ -1,15 +1,16 @@
 public class Main3 {
     public static void main(String[] args) {
-        int month = 0;
+        int month = 12;
+
         if (month <1 || month >12){
             System.out.println("無効な月です");
         } else if (month >=3 && month <=5){
             System.out.println("春");
         } else if (month >=6 && month <=8){
             System.out.println("夏");
-        } else if (month >=12 && month <=2){
+        } else if (month >=12 || month <=2){
             System.out.println("冬");
-        } else if (month>=9 && month <=11){
+        } else if (month >=9 && month <=11){
             System.out.println("秋");
         }else {
             System.out.println("無効な月です");
